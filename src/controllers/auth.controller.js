a6231ecce439faa40dc513d9dedca4cf43e35bad
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-08-13 20:25:16 +0530

    User model with basic registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,9 +6,10 @@ const signup = async (req, res) => {
         const { fullName, username, email, password } = req.body;
         const userExist = await User.findOne({ email });
         if (userExist) {
-            res.status(400).json({ message: "Email already exists!!" })
+            res.status(400).json({ message: "Email already exists!!" });
+        } else {
+            const userCreated = await User.create({ fullName, username, email, password });
         }
-        const userCreated = await User.create({ fullName, username, email, password });
         res.status(200).json({ message: "Registration successful!!" });
     } catch (error) {
         res.status(400).json({ message: "Failed to signup!!" });
