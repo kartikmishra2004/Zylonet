72a4dcc2233d8fabc95f060de5d30e1ffb9b912a
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-08-20 01:55:20 +0530

    bugs fixed

--- Frontend/src/components/PostCard.jsx
+++ Frontend/src/components/PostCard.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import timeAgo from "../utils/TimeFormatter";
 
 const PostCard = ({ title, caption, image, createdAt }) => {
+
+    const [timeStamp, setTimeStamp] = useState(timeAgo(createdAt));
+
+    if (timeStamp === "less than a minute ago") {
+        setTimeStamp("just now");
+    }
+
     return (
         <div>
             <div className="flex flex-col justify-center items-center bg-gray-100 mb-6">
-                <div className="bg-white rounded shadow-md overflow-hidden max-w-lg w-[32rem]">
-                    <img src={image} alt="Mountain" className="w-full h-64 object-contain bg-[#ebebeb]" />
+                <div className="bg-white rounded shadow-md overflow-hidden max-w-lg w-[23rem]">
+                    <img src={image} alt="Mountain" className="w-full h-max object-contain bg-[#ebebeb]" />
                     <div className="p-6">
                         <h2 className="text-2xl font-bold text-gray-800 mb-2">{title}</h2>
                         <p className="text-gray-700 leading-tight mb-4">
@@ -18,7 +25,7 @@ const PostCard = ({ title, caption, image, createdAt }) => {
                                 <a href="#" className="text-gray-500 hover:text-gray-700 mr-4"><i className="far fa-comment-alt"></i> Reply</a>
                                 <a href="#" className="text-gray-500 hover:text-gray-700"><i className="far fa-share-square"></i> Share</a>
                             </div>
-                            <span className="text-gray-600">{timeAgo(createdAt)}</span>
+                            <span className="text-gray-600">{timeStamp}</span>
                         </div>
                     </div>
                 </div>
