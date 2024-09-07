95ab0e818488e20caf8408ba7501cfb3f1b1ce76
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-09-07 16:24:36 +0530

    .

--- Frontend/src/components/Message.jsx
+++ Frontend/src/components/Message.jsx
@@ -6,14 +6,22 @@ import { useAuth } from '../storage/Auth'
 const Message = () => {
 
   const { night } = useAuth()
- 
+
   return (
     <div className={`md:ml-[15rem] md:pl-[3rem] h-screen ${!night ? "bg-[#2a2834] text-[#bababa]" : "bg-white"}`}>
       <NavShadow />
       <HamMenu />
-      <div className='w-full flex justify-center items-center text-2xl'>
-      Comming Soon...
-      </div>
+      <ul role="list" class="divide-y divide-gray-100">
+        <li class="flex justify-between gap-x-6 py-5 px-5 border-b ">
+          <div class="flex min-w-0 gap-x-4">
+            <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+              <div class="min-w-0 flex-auto">
+                <p class="text-sm font-semibold leading-6 text-gray-900">Leslie Alexander</p>
+                <p class="mt-1 truncate text-xs leading-5 text-gray-500">leslie.alexander@example.com</p>
+              </div>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
--- Server/src/controllers/chatlist.controller.js
+++ Server/src/controllers/chatlist.controller.js
@@ -3,14 +3,16 @@ import Chat from "../models/chat.model.js";
 const chatlist = async (req, res) => {
     try {
         const userId = req.user._id;
-        const chatlist = await Chat.find({ _id: { $regex: userId } });
-        if (chatlist) {
+        const chatlist = await Chat.find({ _id: { $regex: userId } })
+            .populate('senderId', 'fullName username');
+
+        if (chatlist.length > 0) {
             res.status(200).json(chatlist);
         } else {
-            res.status(404).json({ message: "No chat found!!" });
+            res.status(404).json({ message: "No chat found!" });
         }
     } catch (error) {
-        res.status(400).json({ message: "Failed to get chatlist!!", error });
+        res.status(400).json({ message: "Failed to get chatlist!", error });
     }
 }
 
