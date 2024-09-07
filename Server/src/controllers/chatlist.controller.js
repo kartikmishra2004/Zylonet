5e84b083eda87e4afa1e0f7022eef5a80bea251e
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-09-07 15:13:08 +0530

    .

--- Server/src/controllers/chatlist.controller.js
+++ Server/src/controllers/chatlist.controller.js
@@ -2,10 +2,18 @@ import Chat from "../models/chat.model.js";
 
 const chatlist = async (req, res) => {
     try {
-        const chatlist = await Chat.find();
-        res.status(200).json(chatlist);
+        const userId = req.user._id;
+        const chatlist = await Chat.find({
+            _id: new RegExp(userId)
+        }).toArray((err, docs) => {
+            if (err) {
+                res.status(400).json({ message: "Failed to get chatlist!!" });
+            } else {
+                res.status(200).json(docs);
+            }
+        });
     } catch (error) {
-        res.status(400).json({ message: "Failed to get chatlist!!" })
+        res.status(400).json({ message: "Failed to get chatlist!!" });
     }
 }
 
