cb02d662f137e222a9607b2f62502c539f878ac4
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-09-07 16:59:18 +0530

    .

--- Server/src/controllers/chatlist.controller.js
+++ Server/src/controllers/chatlist.controller.js
@@ -1,11 +1,31 @@
 import Chat from "../models/chat.model.js";
+import User from "../models/user.model.js";
 
 const chatlist = async (req, res) => {
     try {
         const userId = req.user._id;
-        const chatlist = await Chat.find({ _id: { $regex: userId } })
+        const chatlist = await Chat.find({ _id: { $regex: userId } });
+
         if (chatlist.length > 0) {
-            res.status(200).json(chatlist);
+            // Extract user IDs from chatlist
+            const userIds = chatlist.map(chat => chat._id.split('_')[1]);
+
+            // Fetch user details for those IDs
+            const users = await User.find({ _id: { $in: userIds } });
+
+            // Map users to a dictionary for quick lookup
+            const userMap = users.reduce((acc, user) => {
+                acc[user._id] = { fullName: user.fullName, username: user.username };
+                return acc;
+            }, {});
+
+            // Populate chatlist with user details
+            const populatedChatlist = chatlist.map(chat => ({
+                ...chat.toObject(),
+                user: userMap[chat._id.split('_')[1]] || {}
+            }));
+
+            res.status(200).json(populatedChatlist);
         } else {
             res.status(404).json({ message: "No chat found!" });
         }
