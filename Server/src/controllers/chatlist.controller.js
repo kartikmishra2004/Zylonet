39bc4e80e0f56f6ab66dcf6daa76a2e1cd5d17bb
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-09-07 15:33:50 +0530

    .

--- Server/src/controllers/chatlist.controller.js
+++ Server/src/controllers/chatlist.controller.js
@@ -3,15 +3,12 @@ import Chat from "../models/chat.model.js";
 const chatlist = async (req, res) => {
     try {
         const userId = req.user._id;
-        const chatlist = await Chat.find({
-            _id: new RegExp(userId)
-        }).toArray((err, docs) => {
-            if (err) {
-                res.status(400).json({ message: "Failed to get chatlist!!" });
-            } else {
-                res.status(200).json(docs);
-            }
-        });
+        const chatlist = await Chat.find({ _id: { $regex: userId } });
+        if (chatlist) {
+            res.status(200).json(chatlist);
+        } else {
+            res.status(400).json({ message: "Error!!!" });
+        }
     } catch (error) {
         res.status(400).json({ message: "Failed to get chatlist!!" });
     }
